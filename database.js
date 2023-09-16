de5b41353ed25cd0bf9e2bcf61c92e42a9a330d3
arya javdani <javdani.arya@gmail.com>
2023-09-16 15:36:46 +0330

    final commit

--- database.js
+++ database.js
@@ -1,53 +1,61 @@
-import { Pool } from 'pg';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.queryNoteByContent = exports.queryNoteById = exports.initDB = void 0;
+const pg_1 = require("pg");
 // Initialize the database connection
-const db = new Pool({
+const db = new pg_1.Pool({
     user: 'myuser',
     host: 'localhost',
     database: 'nodejs_db',
     password: 'mypassword',
     port: 5432,
 });
-
-export const initDB = () => {
+const initDB = () => {
     db.connect((err) => {
         if (err) {
             console.error('Cannot connect to psql but aplication will work corroctly without db mode');
-        } else {
+        }
+        else {
             console.log('Connected to PostgreSQL database.');
         }
     });
 };
-
-export const queryNoteById = (id: number) => {
+exports.initDB = initDB;
+const queryNoteById = (id) => {
     db.query('SELECT * FROM notes WHERE id = $1', [id], (err, result) => {
         if (err) {
             console.error('Error querying note by ID:', err);
-        } else {
+        }
+        else {
             const notes = result.rows;
             if (notes.length === 0) {
                 console.log('Note not found.');
-            } else {
+            }
+            else {
                 const note = notes[0];
                 console.log(`Note found - ID: ${note.id}, Content: ${note.content}`);
             }
         }
     });
 };
-export const queryNoteByContent = (id: string) => {
+exports.queryNoteById = queryNoteById;
+const queryNoteByContent = (id) => {
     db.query('SELECT * FROM notes WHERE content = $1', [id], (err, result) => {
         if (err) {
             console.error('Error querying note by content:', err);
-        } else {
+        }
+        else {
             const notes = result.rows;
             if (notes.length === 0) {
                 console.log('Note not found.');
-            } else {
+            }
+            else {
                 const note = notes[0];
                 console.log(`Note found - ID: ${note.id}, Content: ${note.content}`);
             }
         }
     });
 };
+exports.queryNoteByContent = queryNoteByContent;
 // Export the database connection for use in other files
-export default db;
+exports.default = db;
